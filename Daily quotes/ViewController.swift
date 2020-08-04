f769875ebd54b82e69504263b488bc583c1f4979
Abhijot Kaler <akaler@uci.edu>
2020-08-04 16:52:59 -0700

    Initial Commit

---
+++ Daily quotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daily quotes
+//
+//  Created by Abhijot Kaler on 04/08/20.
+//  Copyright © 2020 KalerCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
